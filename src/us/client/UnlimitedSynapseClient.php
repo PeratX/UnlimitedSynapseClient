f93fb6dabeb4bc171bb713b3cead1872358fa7ca
PeratX <1215714524@qq.com>
2016-11-29 23:08:42 +0800

    Init

---
+++ src/us/client/UnlimitedSynapseClient.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * UnlimitedSynapseClient
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PeratX
+ */
+
+namespace us\client;
+
+use sf\module\Module;
+
+class UnlimitedSynapseClient extends Module{
+	public function load(){
+		// TODO: Implement load() method.
+	}
+
+	public function unload(){
+		// TODO: Implement unload() method.
+	}
+}
